d7cf5659f59473ed2eabe56230b9eb34e5dd503f
hmmclean <mclean.haylee@gmail.com>
2023-12-02 17:00:59 -0500

    Create Table_Schema

---
+++ EmployeeSQL/Table_Schemata.sql
@@ -0,0 +1,67 @@
+﻿CREATE TABLE "Employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar(10)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "sex" varchar(1)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Department" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(10)   NOT NULL
+);
+
+CREATE TABLE "Salary" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_Salary" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "EmployeeTitle" (
+    "title_id" varchar(10)   NOT NULL,
+    "title" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_EmployeeTitle" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "Manager" (
+    "dept_no" varchar(10)   NOT NULL,
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_Manager" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "DepartmentName" (
+    "dept_no" varchar(10)   NOT NULL,
+    "dept_name" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_DepartmentName" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "EmployeeTitle" ("title_id");
+
+ALTER TABLE "Department" ADD CONSTRAINT "fk_Department_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Department" ADD CONSTRAINT "fk_Department_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "DepartmentName" ("dept_no");
+
+ALTER TABLE "Salary" ADD CONSTRAINT "fk_Salary_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Manager" ADD CONSTRAINT "fk_Manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "DepartmentName" ("dept_no");
+
+ALTER TABLE "Manager" ADD CONSTRAINT "fk_Manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
